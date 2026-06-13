7c2487e93d1411765a1dcd1700c30a5dd2f708d8
Mehdi Karimi <mehdi-karimi@hotmail.com>
2026-06-13 04:21:29 +0330

    Add VS Code django launch settings and django debuging tool

--- python3/django/hello_world/playground/views.py
+++ python3/django/hello_world/playground/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def calculate():
+    x = 1
+    y = 2
+    return x
+
 # Create your views here.
 def hello_world(request):
     #return HttpResponse('Hello World!')
+    x = calculate()
     return render(request, 'HelloWorld.html', {'name':'Mehdi'})
